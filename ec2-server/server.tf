4c63b14f3b53c0b46719c5873d3b6a210b6a2fe7
hemanth <hemanthtadikonda29@gmail.com>
2023-08-05 08:26:07 +0530

    server dns updated

--- ec2-server/server.tf
+++ ec2-server/server.tf
@@ -1,9 +1,18 @@
-resource "aws_instance" "amazonserver" {
+resource "aws_instance" "amazon" {
     ami = "ami-0f34c5ae932e6f0e4"
     instance_type = "t2.micro"
     security_group_ids = "sg-0baa986d8ebb5eeda"
 
     tags = {
-        name = "amaxon"
+        name = "amazon"
     }
+}
+resource "aws_route53_record" "amazon" {
+
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "amazon.tadikonda.online"
+  type    = "A"
+  ttl     = 30
+  records = [ aws_instance.amazon.private_ip ]
+
 }
