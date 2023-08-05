9f5d9f3e02a560c31642ab56d2e833a560a8ba64
hemanth <hemanthtadikonda29@gmail.com>
2023-08-05 20:03:25 +0530

    roboshop_v1

---
+++ robosho_v1/main.tf
@@ -0,0 +1,198 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0baa986d8ebb5eeda" ]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  name    = "frontend-dev.tadikonda.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0baa986d8ebb5eeda" ]
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  name    = "mongodb-dev.tadikonda.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0baa986d8ebb5eeda" ]
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  name    = "catalogue-dev.tadikonda.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
+resource "aws_instance" "cart" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0baa986d8ebb5eeda" ]
+
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  name    = "cart-dev.tadikonda.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+
+resource "aws_instance" "redis" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0baa986d8ebb5eeda" ]
+
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  name    = "redis-dev.tadikonda.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+
+
+resource "aws_instance" "user" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0baa986d8ebb5eeda" ]
+
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  name    = "user-dev.tadikonda.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0baa986d8ebb5eeda" ]
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  name    = "mysql-dev.tadikonda.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
+resource "aws_instance" "shipping" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0baa986d8ebb5eeda" ]
+
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  name    = "shipping-dev.tadikonda.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0baa986d8ebb5eeda" ]
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  name    = "rabbitmq-dev.tadikonda.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
+resource "aws_instance" "payment" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0baa986d8ebb5eeda" ]
+
+  tags = {
+    Name = "payment"
+  }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  name    = "payment-dev.tadikonda.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
+
+resource "aws_instance" "dispatch" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-0baa986d8ebb5eeda" ]
+
+  tags = {
+    Name = "dispatch"
+  }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  name    = "dispatch-dev.tadikonda.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
