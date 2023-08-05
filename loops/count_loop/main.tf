2ee7d30047adc93b99fa5f7a94ceebbf1831e3dc
hemanth <hemanthtadikonda29@gmail.com>
2023-08-05 14:46:36 +0530

    resource trying with sg

--- loops/count_loop/main.tf
+++ loops/count_loop/main.tf
@@ -1,25 +1,13 @@
 // If we want to iterate same task (resources) multiple time we have a concept called loops//
 
 variable "components" {
-  default = [ "catalogue" , "frontend" , "mongodb" ]
+  default = ["frontend" , "mongodb" ]
 }
 
-resource "aws_instance" "instance" {
-
-    count = length(var.components)
-
-    ami  = "ami-0f34c5ae932e6f0e4"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = ["sg-0baa986d8ebb5eeda"]
-    tags  = {
-      Name  = element(var.components, count.index) // it is list you should mention element // lookup works for Map input
-    }
+resource "aws_security_group" "allow_tls" {
+  count = length(var.components)
+  name  = element(var.components, count.index)
 }
-
-//resource "aws_security_group" "allow_tls" {
-//  count = length(var.components)
-//  name  = element(var.components, count.index)
-//}
 
 
 // while running 2nd time where default = ["catalogue","frontend","mongodb"]
