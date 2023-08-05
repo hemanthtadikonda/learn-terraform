df89ad9142a6befecb91005b43a989577685fdf8
hemanth <hemanthtadikonda29@gmail.com>
2023-08-05 19:42:00 +0530

    for_each loops

--- loops/for_each/main.tf
+++ loops/for_each/main.tf
@@ -1,12 +1,16 @@
 # we know the drawback for count loop so
 variable "components" {
   default  = {
-    frontend = { name = "frontend_sg" }
-    mongodb  = { name = "mongodb_sg"  }
+    frontend = {
+      name = "frontend_sg"
+    }
+    mongodb  = {
+      name = "mongodb_sg"
+    }
   }
 }
 
 resource "aws_security_group" "allow_tls" {
   for_each = var.components
-  name     = lookup(each.value["name"], null)
+  name     = lookup(var.components, each.value["name"], null)
 }
