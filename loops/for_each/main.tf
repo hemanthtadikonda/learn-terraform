afec064f0b979d19321cd689ef7978e503a29d4f
hemanth <hemanthtadikonda29@gmail.com>
2023-08-05 19:51:11 +0530

    for_each loops

--- loops/for_each/main.tf
+++ loops/for_each/main.tf
@@ -4,11 +4,17 @@ variable "components" {
     frontend = { name = "frontend_sg" }
     catalogue = { name = "catalogue_sg" }
     mongodb = { name = "mongodb_sg"  }
+    cart  = {}
+    user  ={}
   }
 }
 
 
 resource "aws_security_group" "allow_tls" {
   for_each = var.components
-  name        = lookup( each.value, "name", null)
-}
+  name        = lookup( each.key, "name", null)  // we have done lot of stunts here like
+}                                     //      like   name        = lookup(var.components, each.value["name"], null)
+
+
+
+// we need get some clarity on how to access the variables  in side a loop as well as outside
