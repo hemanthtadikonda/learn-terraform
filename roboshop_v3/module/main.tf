23c3a3e0a26b1e6659bfb7bbaf54c09b1ed7838b
hemanth <hemanthtadikonda29@gmail.com>
2023-08-06 18:45:34 +0530

    roboshop_v3 with "ansible"

--- roboshop_v3/module/main.tf
+++ roboshop_v3/module/main.tf
@@ -15,3 +15,21 @@ resource "aws_route53_record" "record" {
   ttl     = 30
   records = [aws_instance.instance.private_ip]
 }
+
+resource "null_resource" "ansible" {
+
+  depends_on = [
+    "aws_route53_record.record"
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+  cd /root/Ansible-roboshop
+  git pull
+  sleep 40
+  ansible-playbook -i ${var.name}-dev.tadikonda.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+  EOF
+
+  }
+
+}
