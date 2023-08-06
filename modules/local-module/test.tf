4860326744e2dd2216866d4552e34d6f26a872c8
hemanth <hemanthtadikonda29@gmail.com>
2023-08-06 16:16:32 +0530

    roboshop_v3 with modules

--- modules/local-module/test.tf
+++ modules/local-module/test.tf
@@ -5,13 +5,13 @@ data "aws_ami" "ami" {
 }
 
 output "ami" {
-  value = data.aws_ami.ami.id
+  value = data.aws_ami.ami.id  // here id is attribute before doing anything read documentation once
 }
 
 resource "aws_instance" "instance" {
-  ami           = data.aws_ami.ami.id
+  ami           = data.aws_ami.ami.id  //
   instance_type = var.instance_type
   vpc_security_group_ids = [ "sg-0baa986d8ebb5eeda" ]
 }
 
-variable "instance_type" {}
+variable "instance_type" {}  // here it is collecting variables from root module
