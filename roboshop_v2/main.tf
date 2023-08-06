a75d2050d57f263c39a411843912b670ea71e748
hemanth <hemanthtadikonda29@gmail.com>
2023-08-06 07:53:36 +0530

    "${lookup(each.value,"name",null)}.tadikonda.online"

--- roboshop_v2/main.tf
+++ roboshop_v2/main.tf
@@ -46,7 +46,17 @@ resource "aws_instance" "instance" {
   }
 }
 
-output "checking" {
-  value  = aws_instance.instance
-}
+//output "checking" {
+//  value  = aws_instance.instance
+//}
 
+resource "aws_route53_record" "record" {
+
+  for_each = var.components
+
+  zone_id = var.zone_id
+  name    = "${lookup(each.value,"name",null)}.tadikonda.online"
+  type    = "A"
+  ttl     = 30
+  records = [lookup(lookup(aws_instance.instance,each.key,null),"private_ip",null)]
+}
