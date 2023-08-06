887bef23121d307939151b4c7a64b9b1e27b06f6
hemanth <hemanthtadikonda29@gmail.com>
2023-08-06 07:35:01 +0530

    roboshop_v2 testing

--- roboshop_v2/main.tf
+++ roboshop_v2/main.tf
@@ -17,17 +17,17 @@ variable "security_group" {
 
 variable "components" {
   default = {
-    frontend  = { name = frontend-dev  }
-    mongodb   = { name = mongodb-dev   }
-    catalogue = { name = catalogue-dev }
-    redis     = { name = redis-dev     }
-    user      = { name = user-dev      }
-    cart      = { name = cart -dev     }
-    mysql     = { name = mysql-dev     }
-    shipping  = { name = shipping-dev  }
-    rabbitmq  = { name = rabbitmq-dev  }
-    payment   = { name = payment-dev   }
-    dispatch  = { name = dispatch-dev  }
+    frontend  = { name = "frontend-dev"  }
+    mongodb   = { name = "mongodb-dev "  }
+    catalogue = { name = "catalogue-dev" }
+    redis     = { name = "redis-dev "    }
+    user      = { name = "user-dev"      }
+    cart      = { name = "cart-dev"     }
+    mysql     = { name = "mysql-dev"     }
+    shipping  = { name = "shipping-dev"  }
+    rabbitmq  = { name = "rabbitmq-dev"  }
+    payment   = { name = "payment-dev"   }
+    dispatch  = { name = "dispatch-dev"  }
 
   }
 }
