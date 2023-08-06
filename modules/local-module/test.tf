ac1e6fe9a87dc60a92a759a6040a500826569d08
hemanth <hemanthtadikonda29@gmail.com>
2023-08-06 13:11:01 +0530

    modules

--- modules/local-module/test.tf
+++ modules/local-module/test.tf
@@ -11,7 +11,7 @@ output "ami" {
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ "sg-0dee954b08055e577" ]
+  vpc_security_group_ids = [ "sg-0baa986d8ebb5eeda" ]
 }
 
 variable "instance_type" {}
