fa03fdc0fed93c3a2e7bd43c5fe61504d733580f
hemanth <hemanthtadikonda29@gmail.com>
2023-08-06 10:15:23 +0530

    checking for output

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,7 @@
 module "test" {                   // calling test module in local path
   source = "./local-module"
 }
+
+output "ami" {
+  value = "module.test"     // module.test.ami - you can get only ami output date here
+}                       // out put inside the child module can't directly print on root module
