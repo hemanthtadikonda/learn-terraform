2e25efdea5d1f46a65bb0ffdc0f74d5fbd1c0688
hemanth <hemanthtadikonda29@gmail.com>
2023-08-06 07:30:37 +0530

    roboshop_v2 testing

---
+++ roboshop_v2/main.tf
@@ -0,0 +1,52 @@
+
+variable "zone_id" {
+  default  = "Z09760323G7SC2VABFTOY"
+}
+
+variable "ami" {
+  default  = "ami-03265a0778a880afb"
+}
+
+variable "instance_type" {
+  default  = "t2.micro"
+}
+
+variable "security_group" {
+  default  = "sg-0baa986d8ebb5eeda"
+}
+
+variable "components" {
+  default = {
+    frontend  = { name = frontend-dev  }
+    mongodb   = { name = mongodb-dev   }
+    catalogue = { name = catalogue-dev }
+    redis     = { name = redis-dev     }
+    user      = { name = user-dev      }
+    cart      = { name = cart -dev     }
+    mysql     = { name = mysql-dev     }
+    shipping  = { name = shipping-dev  }
+    rabbitmq  = { name = rabbitmq-dev  }
+    payment   = { name = payment-dev   }
+    dispatch  = { name = dispatch-dev  }
+
+  }
+}
+
+
+resource "aws_instance" "instance" {
+
+
+
+  ami                     = var.ami
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = var.security_group
+
+  tags            = {
+    Name          = lookup(var.components, payment["name"], null )
+  }
+}
+
+output "checking" {
+  value  = aws_instance.instance
+}
+
