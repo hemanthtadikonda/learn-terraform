36219e1940954b809962cfb5e41b10563caed036
hemanth <hemanthtadikonda29@gmail.com>
2023-08-06 18:48:21 +0530

    roboshop_v3 with "ansible"

--- roboshop_v3/module/main.tf
+++ roboshop_v3/module/main.tf
@@ -19,7 +19,7 @@ resource "aws_route53_record" "record" {
 resource "null_resource" "ansible" {
 
   depends_on = [
-    "aws_route53_record.record"
+    aws_route53_record.record
   ]
 
   provisioner "local-exec" {
